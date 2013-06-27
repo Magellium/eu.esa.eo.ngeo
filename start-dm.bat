bdcc13531bcd70932da1f90b4bcbb45ddf07952c
Yeggstry <yeggstry@gmail.com>
2013-06-27 09:04:56 +0000

    Reference a truststore that *should* contain SSL certs needed for accessing UM-SSO-protected resources. Note that, at present, such a truststore can be created for GMV's test environment using ssl_certs.bat but must be manually renamed and moved/copied to the location referenced from this script.

--- start-dm.bat
+++ start-dm.bat
@@ -6,4 +6,10 @@ SET DM_HOME=%~dp0
 
 REM SET DEBUG_OPTIONS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000
 
-"%DM_HOME%jre\jre1.7.0_21\bin\java" -DDM_HOME=%DM_HOME% %DEBUG_OPTIONS% -jar "%DM_HOME%bin\download-manager-webapp-jetty-console.war" --headless --port 8082 --contextPath /download-manager
+REM In the following command, we put a literal '\' after  _DDM_HOME="%DM_HOME%  because %DM_HOME% has a trailing slash and java.exe uses '\' as the character to escape double quote characters and backslashes.
+set MYCOMMAND="%DM_HOME%jre\jre1.7.0_21\bin\java" -Djavax.net.ssl.trustStore="C:\temp\dmtu-truststore.jks" -DDM_HOME="%DM_HOME%\" %DEBUG_OPTIONS% -jar "%DM_HOME%bin\download-manager-webapp-jetty-console.war" --headless --port 8082 --contextPath /download-manager
+
+echo Command=%MYCOMMAND%
+echo
+
+%MYCOMMAND%
