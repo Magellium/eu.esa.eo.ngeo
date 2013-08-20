5dc3f9f89d2084692ba3d468c7e2c64b0029a878
Yeggstry <yeggstry@gmail.com>
2013-08-20 10:59:49 +0000

    Updated ssl certs batch file to accept IDP and SP certificates as arguments. This allows for greater flexibility.

--- src/main/scripts/ssl_certs.bat
+++ src/main/scripts/ssl_certs.bat
@@ -1,13 +1,16 @@
-REM You may need to run this as Administrator
-REM Run this batch file only from the directory that contains it. (This is because relative paths are used.)
+@echo off
 
-@echo ON
+if [%1]==[] goto usage
+if [%2]==[] goto usage
+
+echo You may need to run this as Administrator
+echo Run this batch file only from the directory that contains it. (This is because relative paths are used.)
 
 set OPENSSL_BIN_DIR=D:\Programs\OpenSSL-Win64\bin
 set OPENSSL_EXE=%OPENSSL_BIN_DIR%\openssl.exe
 set OPENSSL_CONF=%OPENSSL_BIN_DIR%\openssl.cfg
 
-set JAVA_HOME=D:\Programs\Java\jdk1.7.0_03
+set JAVA_HOME="C:\Program Files\Java\jdk1.7.0_21"
 set JAVA_BIN=%JAVA_HOME%\bin
 set KEYTOOL_EXE=%JAVA_BIN%\keytool.exe
 
@@ -15,12 +18,9 @@ set APP_NAME=DMTU
 set KEYSTORE_FILENAME_P12=%APP_NAME%.p12
 set KEYSTORE_FILENAME_JKS=%APP_NAME%.jks
 set KEYSTORE_PASSWORD=whatever
-set GMV_TEST_IDP_CERTIFICATE_FILEPATH=..\resources\certs\umsso.pem
 
-
-REM NB: The following is probably partly mis-named; although Ian obtained it using Firefox, it's surely a reference to the *SP's* certificate. 
-set GMV_TEST_IDP_CERTIFICATE_FILEPATH_FROM_BROWSER=..\resources\certs\webs.novalocal.crt
-
+set IDP_CERTIFICATE_FILEPATH=%1
+set SP_CERTIFICATE_FILEPATH=%2
 
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------
 echo Generate the client truststore file
@@ -34,7 +34,7 @@ IF NOT exist %TEMP_DIR% (
 	del /Q %TEMP_DIR%\*.*
 )
 
-set CLIENT_TRUSTSTORE_FILE_PATHNAME=..\..\..\target\temp\client-truststore.jks
+set CLIENT_TRUSTSTORE_FILE_PATHNAME=..\..\..\target\temp\dmtu-truststore.jks
 set MERGED=merged_client-truststore.jks
 
 REM The StackOverflow article used "keytool -genkey" which, in Java 6, was replaced by Sun/Oracle with "keytool -genkeypair".
@@ -45,8 +45,8 @@ REM @echo OFF
 echo You are about to be prompted for the keystore password twice; type '%KEYSTORE_PASSWORD%' each time.
 REM The following keytool command rejects non-X509 certificates
 REM @echo ON
-%KEYTOOL_EXE% -import -keystore %CLIENT_TRUSTSTORE_FILE_PATHNAME% -file %GMV_TEST_IDP_CERTIFICATE_FILEPATH%               -alias IDP  || goto :error
-%KEYTOOL_EXE% -import -keystore %CLIENT_TRUSTSTORE_FILE_PATHNAME% -file %GMV_TEST_IDP_CERTIFICATE_FILEPATH_FROM_BROWSER%  -alias IDP2 || goto :error
+%KEYTOOL_EXE% -import -keystore %CLIENT_TRUSTSTORE_FILE_PATHNAME% -file %IDP_CERTIFICATE_FILEPATH% -alias IDP  || goto :error
+%KEYTOOL_EXE% -import -keystore %CLIENT_TRUSTSTORE_FILE_PATHNAME% -file %SP_CERTIFICATE_FILEPATH%  -alias SP || goto :error
 REM @echo OFF
 
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------
@@ -68,3 +68,7 @@ exit /B
 :error
 echo Failed with error code %errorlevel%.
 exit /B %errorlevel%
+
+:usage
+echo Usage: %0 ^<IDP_FILEPATH^> ^<SP_FILEPATH^>
+exit /B 1
