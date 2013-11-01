02817687b0ff51caaa36d27b175a18d3dc56bc71
Yeggstry <yeggstry@gmail.com>
2013-11-01 14:17:39 +0000

    Updated plugin version in preperation for new release.

--- download-manager-plugin-generic/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/HTTPDownloadPluginInfo.java
+++ download-manager-plugin-generic/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/HTTPDownloadPluginInfo.java
@@ -9,7 +9,7 @@ public class HTTPDownloadPluginInfo implements IDownloadPluginInfo {
 	}
 
 	public int[] getPluginVersion() {
-		return new int[] { 0, 5, 0};
+		return new int[] { 0, 5, 1};
 	}
 
 	public String[] getMatchingPatterns() {
@@ -17,7 +17,7 @@ public class HTTPDownloadPluginInfo implements IDownloadPluginInfo {
 	}
 
 	public int[] getDMMinVersion() {
-		return new int[] { 0, 5, 0};
+		return new int[] { 0, 5, 1};
 	}
 
 	public boolean handlePause() {
