094fe7fb530e2ebbd522ad8f55456e136bc02cda
Yeggstry <yeggstry@gmail.com>
2013-12-04 15:21:20 +0000

    Update plugin version in preperation of release.

--- download-manager-plugin-generic/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/HTTPDownloadPluginInfo.java
+++ download-manager-plugin-generic/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/HTTPDownloadPluginInfo.java
@@ -9,7 +9,7 @@ public class HTTPDownloadPluginInfo implements IDownloadPluginInfo {
 	}
 
 	public int[] getPluginVersion() {
-		return new int[] { 0, 5, 5};
+		return new int[] { 0, 5, 6};
 	}
 
 	public String[] getMatchingPatterns() {
@@ -17,7 +17,7 @@ public class HTTPDownloadPluginInfo implements IDownloadPluginInfo {
 	}
 
 	public int[] getDMMinVersion() {
-		return new int[] { 0, 5, 5};
+		return new int[] { 0, 5, 6};
 	}
 
 	public boolean handlePause() {
--- download-manager-plugin-mock-sentinel2/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/Sentinel2DownloadPluginInfo.java
+++ download-manager-plugin-mock-sentinel2/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/Sentinel2DownloadPluginInfo.java
@@ -17,7 +17,7 @@ public class Sentinel2DownloadPluginInfo implements IDownloadPluginInfo {
 	}
 
 	public int[] getDMMinVersion() {
-		return new int[] { 0, 5, 5};
+		return new int[] { 0, 5, 6};
 	}
 
 	public boolean handlePause() {
