f17ad2724e324d4cdc1a7a74ef1860dd77c14a5d
Yeggstry <yeggstry@gmail.com>
2013-12-16 15:37:02 +0000

    Improved logging message from the incorrect parsing of an unexpected response.

--- download-manager-webapp/src/main/java/int_/esa/eo/ngeo/dmtu/webserver/builder/NgeoWebServerResponseParser.java
+++ download-manager-webapp/src/main/java/int_/esa/eo/ngeo/dmtu/webserver/builder/NgeoWebServerResponseParser.java
@@ -77,7 +77,7 @@ public class NgeoWebServerResponseParser {
 
 					throw new WebServerServiceException(String.format("%s. Reason was: %s", httpResponseMessage, exceptionReport.getErrorDetail()));
 				}catch(ParseException e) {
-					throw new WebServerServiceException(String.format("Unable to parse the response to a request for %s: HTTP response code %s; \"%s\".",
+					throw new WebServerServiceException(String.format("Unexpected response to a request for %s: HTTP response code %s; \"%s\".",
 							serviceUrl.toString(), httpStatusCode, httpResponseMessage));
 				}
 			}
