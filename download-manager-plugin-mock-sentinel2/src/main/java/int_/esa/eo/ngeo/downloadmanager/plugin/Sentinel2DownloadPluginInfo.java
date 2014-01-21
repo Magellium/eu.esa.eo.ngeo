ebdc8e76caeab79a5d6ca0b8506110f99cb6a367
Yeggstry <yeggstry@gmail.com>
2014-01-21 17:00:23 +0000

    Updated release note and plugin min version in preperation for new release.

--- download-manager-plugin-mock-sentinel2/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/Sentinel2DownloadPluginInfo.java
+++ download-manager-plugin-mock-sentinel2/src/main/java/int_/esa/eo/ngeo/downloadmanager/plugin/Sentinel2DownloadPluginInfo.java
@@ -17,7 +17,7 @@ public class Sentinel2DownloadPluginInfo implements IDownloadPluginInfo {
 	}
 
 	public int[] getDMMinVersion() {
-		return new int[] { 0, 5, 9};
+		return new int[] { 0, 6, 0};
 	}
 
 	public boolean handlePause() {
